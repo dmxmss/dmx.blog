9aad2ed64e3cf6751900639d5f68c59c4b187f23
dmxmss <dexammt@gmail.com>
2024-09-30 19:42:53 +0000

    article dates & new method

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use serde::Serialize;
 use rocket_dyn_templates::Template;
+use chrono::offset::Utc;
 
 #[derive(Serialize)]
 struct Context {
@@ -12,29 +13,30 @@ struct Context {
 struct Article {
     id: u64,
     name: String,
-    contents: String
+    contents: String,
+    pub_date: String,
+    edit_date: String
+}
+
+impl Article {
+    fn new(id: u64, name: String, contents: String) -> Article {
+        Article {
+            id, 
+            name, 
+            contents, 
+            pub_date: Utc::now().to_string(), 
+            edit_date: Utc::now().to_string()
+        }
+    }
 }
 
 #[get("/")]
 fn index() -> Template {
     let context = Context {
         articles: vec![
-            Article {
-                id: 1, 
-                name: String::from("First article"), 
-                contents: String::from("Hello, world")
-            }, 
-            Article {
-                id: 2, 
-                name: String::from("Second article"), 
-                contents: String::from("Hello, world")
-            }, 
-            Article {
-                id: 3, 
-                name: String::from("Third article"), 
-                contents: String::from("Hello, world")
-            }, 
-
+            Article::new(1, String::from("First article"), String::from("Hello world")),
+            Article::new(2, String::from("Second article"), String::from("Hello world")),
+            Article::new(3, String::from("Third article"), String::from("Hello world"))
         ]
     };
 
