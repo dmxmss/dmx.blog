54f71f9cea62a4435248c7d684cce520f0a0939b
dmxmss <dexammt@gmail.com>
2024-10-01 18:53:26 +0000

    read articles from articles.json

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #[macro_use] extern crate rocket;
 
-use serde::Serialize;
-use rocket_dyn_templates::Template;
+use serde::{Deserialize, Serialize};
+use rocket_dyn_templates::{Template, context};
 use chrono::offset::Utc;
+use std::fs;
 
 #[derive(Serialize)]
 struct Context {
     articles: Vec<Article>
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct Article {
     id: u64,
     name: String,
@@ -32,15 +33,16 @@ impl Article {
 
 #[get("/")]
 fn index() -> Template {
-    let context = Context {
-        articles: vec![
-            Article::new(1, String::from("First article"), String::from("Hello world")),
-            Article::new(2, String::from("Second article"), String::from("Hello world")),
-            Article::new(3, String::from("Third article"), String::from("Hello world"))
-        ]
-    };
+    // let context = Context {
+    //     articles: vec![
+    //         Article::new(1, String::from("First article"), String::from("Hello world")),
+    //         Article::new(2, String::from("Second article"), String::from("Hello world")),
+    //         Article::new(3, String::from("Third article"), String::from("Hello world"))
+    // };
 
-    Template::render("index", context)
+    let articles: Vec<Article> = serde_json::from_str(fs::read_to_string("articles.json").unwrap().as_str()).unwrap();
+
+    Template::render("index", context! {articles: articles})
 }
 
 #[launch]
